07f48401b1bfb85eb5855c8312d3582f0bf245b3
buratsakon0 <buratsakon_s@cmu.ac.th>
2024-09-10 18:35:17 +0700

    lab completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,7 @@ import {
 
 import { Form, useForm, zodResolver } from "@mantine/form";
 import { z } from "zod";
+import path from "path";
 
 const schema = z
   .object({
@@ -48,7 +49,7 @@ const schema = z
     }),
     hasCoupon: z.boolean(),
     coupon: z.string(),
-    password: z.string(),
+    password: z.string().min(6, { message: "Password must contain at least 6 characters"}).max(12, {message: "“Password must not exceed 12 characters"}),
     confirmPassword: z.string(),
   })
   .refine(
@@ -68,6 +69,17 @@ const schema = z
     {
       message: "Invalid coupon code",
       path: ["coupon"],
+    }
+
+    
+  ).refine(
+    (pass) => {
+      if (pass.password !== pass.confirmPassword) return false;
+      return true;
+    },
+    {
+      message: "Password does not match",
+      path: ["confirmPassword"],
     }
   );
 
@@ -96,10 +108,14 @@ export default function Home() {
     //TIP : get value of currently filled form with variable "form.values"
 
     if (form.values.plan === "funrun") price = 500;
+    if (form.values.plan === "mini") price = 800;
+    if (form.values.plan === "half") price = 1200;
+    if (form.values.plan === "full") price = 1500;
     //check the rest plans by yourself
     //TIP : check /src/app/libs/runningPlans.js
 
     //check discount here
+    if (form.values.hasCoupon && form.values.coupon === "CMU2023") price *= 0.7; // apply 30% discount
 
     return price;
   };
@@ -187,7 +203,7 @@ export default function Home() {
           </Stack>
         </form>
 
-        {/* <Footer year={2023} fullName="Chayanin Suatap" studentId="650610560" /> */}
+        <Footer year={2024} fullName="Buratsakon Srikhaw" studentId="660612150" />
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
--- src/libs/types.ts
+++ src/libs/types.ts
@@ -4,5 +4,8 @@ export interface termsAndCondsProps {
 }
 
 export interface footerProps {
-  // define props here
+  year: number;
+  fullName: string;
+  studentId: string;
 }
+
